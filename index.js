928797a7e17b4c323e5a74c9cfc19265c95a3345
Igor Gabriel <igorgabrielma@gmail.com>
2022-10-10 21:22:41 -0300

    Finalizado CRUD de livros da biblioteca-api

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const bodyParser = require('body-parser')
 const app = express()
 const db = require('./db')
 const port = 3000
+const Book = require('./book')
 
 app.use(bodyParser.json())
 
-app.post('/v1/books', async (req, res, next) => {
-    const Book = require('./book')
+app.get('/', (req, res) => {
+    res.send('biblioteca-api is running')
+})
+
+app.post('/v1/books', async (req, res, next) => {  
     try {
         const data = req.body
         const newBook = new Book(data)
@@ -25,8 +29,57 @@ app.post('/v1/books', async (req, res, next) => {
 
 })
 
-app.get('/', (req, res) => {
-    res.send('biblioteca-api is running')
+app.get('/v1/books', async (req, res, next) => {
+    try {
+        const books = await Book.find()
+        res.status(200).json(books)
+    } catch(err) {
+        next(err)
+    }
+})
+
+app.get('/v1/books/:id', async (req, res, next) => {
+    try {
+        const id = req.params.id
+        const book = await Book.findById(id)
+        if(!book) {
+            throw new Error(`Book with id ${id} not found`)
+        }
+        res.status(200).json(book)
+    } catch(err) {
+        next(err)
+    }
+})
+
+app.put('/v1/books/:id', async (req, res, next) => {
+    try {
+        const id = req.params.id
+        const data = req.body
+
+        const book = await Book.findById(id)
+        if(!book) {
+            throw new Error(`Book with id ${id} not found`)
+        }
+
+        const newBook = await Book.findByIdAndUpdate(id, data, {new: true})
+        res.status(200).json(newBook)
+    } catch(err) {
+        next(err)
+    }
+})
+
+app.delete('/v1/books/:id', async (req, res, next) => {
+    try {
+        const id = req.params.id
+        const book = await Book.findById(id)
+        if(!book) {
+            throw new Error(`Book with id ${id} not found`)
+        }
+        await Book.findByIdAndDelete(id)
+        res.status(200).json({message: `Book with id ${id} has deleted`})
+    } catch(err) {
+        next(err)
+    }
 })
 
 app.use((error, req, res, next) => {
